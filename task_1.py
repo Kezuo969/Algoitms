4333b2da67117695a3002192e88d730bdb37bad1
Kezuo969 <73736154+Kezuo969@users.noreply.github.com>
2020-11-07 15:27:24 +0300

    Lesson-3

--- task_1.py
+++ task_1.py
@@ -1,64 +1,36 @@
 """
-1.	Написать программу, которая будет складывать, вычитать, умножать или делить
-два числа. Числа и знак операции вводятся пользователем. После выполнения
-вычисления программа не должна завершаться, а должна запрашивать новые данные
-для вычислений. Завершение программы должно выполняться при вводе символа '0'
-в качестве знака операции. Если пользователь вводит неверный знак
-(не '0', '+', '-', '*', '/'), то программа должна сообщать ему об ошибке и
-снова запрашивать знак операции.
+Задание 1.
 
-Также сообщать пользователю о невозможности деления на ноль,
-если он ввел 0 в качестве делителя.
+Докажите, что словари обрабатываются быстрее, чем списки.
 
-Подсказка:
-Вариант исполнения:
-- условие рекурсивного вызова - введена операция +, -, *, / - ШАГ РЕКУРСИИ
-- условие завершения рекурсии - введена операция 0 - БАЗОВЫЙ СЛУЧАЙ
+Реализуйте две функции, в первой нужно заполнить элементами список, во второй-словарь
+Сделайте замеры времени выполнения каждой из функций
 
-Решите через рекурсию. Решение через цикл не принимается.
-Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
+Подсказка: для замеров воспользуйтесь модулем time (см. примеры урока 1)
 
-Пример:
-Введите операцию (+, -, *, / или 0 для выхода): +
-Введите первое число: 214
-Введите второе число: 234
-Ваш результат 448
-Введите операцию (+, -, *, / или 0 для выхода): -
-Введите первое число: вп
-Вы вместо трехзначного числа ввели строку (((. Исправьтесь
-Введите операцию (+, -, *, / или 0 для выхода):
+Примечание: eсли вы уже знаете, что такое декоратор и как его реализовать,
+то реализуйте ф-цию-декоратор и пусть она считает время
+И примените ее к двум своим функциям.
 """
 
-class Script(Exception):
-    def __init__(self, data, sheld):
-        self.data = data
-        sheld = []
-        data = input("Enter number to exit enter 'stop'").lower()
-        if data !="stop":
-            if data.isdigit() == True:
-                sheld.append(data)
-                data = input("Enter number to exit enter 'stop'")
+def timefunc(func):
+    import time
+    
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end-start))
+    return wrapper
 
+@timefunc
+def listfunc():
+    a= []
+    a= [i for i in a(range(10000))] 
+listfunc()
 
-
-def menu():
-    print ("Welcome to calculator.py")
-    print ("your options are:") pr
-    int (" ") print ("1) Addition")
-    print ("2) Subtraction")
-    print ("3) Multiplication")
-    print ("4) Division")
-    print ("5) Quit calculator.py")
-    print (" ")
-    return input ("Choose your option: ")
-
-    def add(a,b): print (a, "+", b, "=", a + b)
-    def sub(a,b): print (b, "-", a, "=", b - a)
-    def mul(a,b): print (a, "*", b, "=", a * b)
-    def div(a,b): print (a, "/", b, "=", a / b) loop = 1 choice = 0
-    while loop == 1: choice = menu()
-    if choice == 1: add(input("Add this: "),input("to this: "))
-    elif choice == 2: sub(input("Subtract this: "),input("from this: "))
-    elif choice == 3: mul(input("Multiply this: "),input("by this: "))
-    elif choice == 4: div(input("Divide this: "),input("by this: "))
-    elif choice == 5: loop = 0 print ("Thank you for using calculator.py!")
+@timefunc
+def vocfunc():
+    v= {}
+    v= [i for i in v(range(10000))] 
+vocfunc()
